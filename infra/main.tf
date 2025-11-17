cc5f88f3ebc088c8604025f58db0c3fc8983da09
Mykola Mateichuk <mykola.mateichuk.dev@gmail.com>
2025-11-17 21:04:57 +0200

    Add punlic IPs on launch

--- infra/main.tf
+++ infra/main.tf
@@ -7,8 +7,9 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_internet_gateway" "gw" {
